10bea844a884b6ff3b97e689fe134aa57e87b745
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2022-12-17 23:39:44 +0500

    services html end

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// var userName = prompt("ismingizni kiriting");
+// alert(`Alaykum ${userName}`);
+
+// var birthDay = prompt("Yoshingizni kiriting");
+// alert(`Sizning yoshingiz ${birthDay}`);
+// var firstName = "Shodiyor";
+// var job = "developer";
+// var birthDate = 2004;
+// var currentYear = 2022;
+// var template = `mening ismim ${firstName}, yoshim ${currentYear - birthDate} da`
+// console.log(template)
+
+// var birthDate = prompt("yoshingizni kiriting ");
+// alert(` ${birthDate} `);
+// var a ;
+
+// if (birthDate > 18) {
+//     console.log("sizning yoshingiz yetadi")
+// } else if (birthDate < 18) {
+//    a = 18 - birthDate 
+//     console.log(` shuncha yildan so'ng yoshingiz yetadi,${a} `);
+// }
+
+// var birthDate = prompt (" tug'ilgan yilingizni kiriting")
+
+// if( birthDate >= 2000 && birthDate <= 2099)
+// {
+//     console.log( "siz 21 asrda tug'ilgansiz")
+// }
+// else if (birthDate >= 1900 && birthDate <= 1999) {
+// console.log("siz 20 asrda tug'ilgansiz")
+// }
+// else{
+//     console.log("siz boshqa asrda tug'ilgansiz")
+// }
